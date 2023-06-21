ef9ea16fe5d351f809f1b784fb851f4429c34c6e
Denis Bebikh <denisbebikh@gmail.com>
2023-06-22 00:33:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
-
+from flask import Flask, request
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Render Test Successful!"
+@app.route("/add", methods=['GET'])
+def add():
+    num1 = float(request.args.get('num1', 0))
+    num2 = float(request.args.get('num2', 0))
+    result = num1 + num2
+    return {'result': result}
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)

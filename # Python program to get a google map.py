835f5e3a14ab39873792703a09418a874c64b900
Denis Bebikh <denisbebikh@gmail.com>
2023-06-21 23:45:18 +0300

    Update # Python program to get a google map.py

--- # Python program to get a google map.py
+++ # Python program to get a google map.py
@@ -1,37 +1,10 @@
-# Python program to get a google map
-# image of specified location using
-# Google Static Maps API
- 
-# importing required modules
-import requests
- 
-# Enter your api key here
-api_key = "AIzaSyCntFnxggCr5VFc3FDjRLCe5QYvdIRLOds"
- 
-# url variable store url
-url = "https://maps.googleapis.com/maps/api/staticmap?"
- 
-# center defines the center of the map,
-# equidistant from all edges of the map.
-center = "Dehradun"
- 
-# zoom defines the zoom
-# level of the map
-zoom = 10
- 
-# get method of requests module
-# return response object
-r = requests.get(url + "center =" + center + "&zoom =" +
-                   str(zoom) + "&size = 400x400&key =" +
-                             api_key + "sensor = false")
- 
-# wb mode is stand for write binary mode
-f = open('address of the file location ', 'wb')
- 
-# r.content gives content,
-# in this case gives image
-f.write(r.content)
- 
-# close method of file object
-# save and close the file
-f.close()
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def map_display():
+    return render_template('map.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
